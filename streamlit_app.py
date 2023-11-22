e604b029febd894e141b70650f8380ec34d89103
Mauricio Maciel <mbmaciel@outlook.com>
2023-11-22 13:17:57 +0000

    só gráfico

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,30 +2,25 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import array
 
 """
 # Bem vindo ao exemplo de Streamlit do Mbmaciel!
 
 Abaixo os dados do banco ecletica.
 Os dados foram capturados com cache.
-
 """
-
-# Initialize connection.
+data = []
 conn = st.connection('mysql', type='sql')
-
-df = conn.query('SELECT * from ecletica_financeiro;', ttl=600)
-
+df = conn.query('SELECT * from ecletica_financeiro limit 10;', ttl=600)
 
 # num_ticket, data_hora_abre, data_hora_fecha, vlr_total, dinheiro
 for row in df.itertuples():
-    df1 = pd.DataFrame({row.vlr_total}, columns=("col %d" % i for i in range(20)))
+    data.append({row.vlr_total})
+    chart_data = pd.DataFrame(data, columns=["valor"])
+    #st.write(f"{row.num_ticket} , {row.data_hora_abre}, {row.data_hora_fecha}, {row.vlr_total}, {row.dinheiro} ")
 
-    st.write(f"{row.num_ticket} , {row.data_hora_abre}, {row.data_hora_fecha}, {row.vlr_total}, {row.dinheiro} ")
-
-my_table = st.table(df1)
-
-my_table.add_rows(df1)
+st.bar_chart(chart_data)
 
 
 
