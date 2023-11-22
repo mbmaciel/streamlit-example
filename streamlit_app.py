bfbb38d532b44143701fd7f06c27886e9cf8a320
Mauricio Maciel <mbmaciel@outlook.com>
2023-11-22 13:37:18 +0000

    grafico mais complexo

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,25 @@ Abaixo os dados do banco ecletica.
 Os dados foram capturados com cache.
 """
 data = []
+hora = []
 conn = st.connection('mysql', type='sql')
 df = conn.query('SELECT * from ecletica_financeiro limit 10;', ttl=600)
 
 # num_ticket, data_hora_abre, data_hora_fecha, vlr_total, dinheiro
 for row in df.itertuples():
     data.append({row.vlr_total})
-    chart_data = pd.DataFrame(data, columns=["valor"])
+    hora.append({row.data_hora_abre})
+    chart_data = pd.DataFrame(
+        {
+            "col1": data,
+            "col2": hora,
+            
+        }
+    )
+    
     #st.write(f"{row.num_ticket} , {row.data_hora_abre}, {row.data_hora_fecha}, {row.vlr_total}, {row.dinheiro} ")
 
-st.bar_chart(chart_data)
+st.bar_chart(chart_data, x="col1", y="col2")
 
 
 
